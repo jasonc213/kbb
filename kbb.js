9bbb2abee80a2100c6f9a355d3ed8b7b8ba7bed6
Jason Coeyman <jason.coeyman@gmail.com>
2014-08-23 19:39:14 -0400

    updated

--- kbb.js
+++ kbb.js
@@ -4,23 +4,29 @@
 
 function generate() {
 
+	// Sets which boards are available and sets placeholder for chosen boards
 	var availBoards = ["paddock", "farm", "tower", "oasis", "barn", "tavern"]
-	var currentBoardIndex = Math.floor(Math.random() * availBoards.length);
-	var board1 = availBoards[currentBoardIndex];
-	availBoards.splice(currentBoardIndex, 1);
-	currentBoardIndex = Math.floor(Math.random() * availBoards.length);
-	var board2 = availBoards[currentBoardIndex];
-	availBoards.splice(currentBoardIndex, 1);
-	currentBoardIndex = Math.floor(Math.random() * availBoards.length);
-	var board3 = availBoards[currentBoardIndex];
-	availBoards.splice(currentBoardIndex, 1);
-	currentBoardIndex = Math.floor(Math.random() * availBoards.length);
-	var board4 = availBoards[currentBoardIndex];
-
-	document.getElementById("Board1").innerHTML = board1;
-	document.getElementById("Board2").innerHTML = board2;
-	document.getElementById("Board3").innerHTML = board3;
-	document.getElementById("Board4").innerHTML = board4;
-
+	var chosenBoards = ["board1", "board2", "board3", "board4"];
 	
+	//Cycles through and pulls out 4 boards randomly
+	for (var x = 0; x < 4; x++) {
+		var currentBoardIndex = Math.floor(Math.random() * availBoards.length);
+		chosenBoards[x] = availBoards[currentBoardIndex];
+		availBoards.splice(currentBoardIndex, 1);
+	};
+	
+	//outputs the boards to the html file
+	document.getElementById("Board1").innerHTML = chosenBoards[0] + " " + isFlipped();
+	document.getElementById("Board2").innerHTML = chosenBoards[1] + " " + isFlipped();
+	document.getElementById("Board3").innerHTML = chosenBoards[2] + " " + isFlipped();
+	document.getElementById("Board4").innerHTML = chosenBoards[3] + " " + isFlipped();
+};
+
+//flips a coin to see if the board should be flipped or not
+function isFlipped() {
+	if (Math.floor(Math.random() * 2) === 1) {
+		return "flipped";
+	} else {
+		return "";
+	};
 };
